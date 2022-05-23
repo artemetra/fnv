c99848930bdb9cfd9fc608fad74b1b955021104b
artemetra <artemlukin15@gmail.com>
2022-05-23 23:16:51 +0200

    generalized fnvreaderror

--- fnvcli/src/file_reader.rs
+++ fnvcli/src/file_reader.rs
@@ -4,11 +4,16 @@ pub mod file_reader {
     use std::fmt;
 
     #[derive(Debug)]
-    pub enum FnvReadError {
+    pub struct FnvReadError {
+        pub kind: FnvReadErrorKind,
+    }
+
+    #[derive(Debug)]
+    pub enum FnvReadErrorKind {
         /// file size too small to process
         FileTooSmall,
         /// invalid curve type in file
-        InvalidCurveType,
+        InvalidCurveType(u8),
     }
 
     impl fmt::Display for FnvReadError {
@@ -16,16 +21,25 @@ pub mod file_reader {
             write!(f, "{:?}", self)
         }
     }
+    impl fmt::Display for FnvReadErrorKind {
+        fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+            write!(f, "{:?}", self.to_string())
+        }
+    }
 
     impl Error for FnvReadError {}
 
-    pub fn read_fnv_file(_file: &Vec<u8>) -> Result<(), Box<dyn Error>> {
-        if _file.len() < 56 { // smallest size i've found
+    pub fn read_fnv_file(_file: &Vec<u8>) -> Result<(), FnvReadError> {
+        // smallest size i've found
+        if _file.len() < 56 {
+            return Err(FnvReadError {
+                kind: FnvReadErrorKind::FileTooSmall,
+            });
         }
         let mut file_iter = _file.iter();
 
         let curve_type = curve_type(file_iter.next().unwrap())?;
-
+        
         Ok(()) // should be removed
     }
 
@@ -35,7 +49,9 @@ pub mod file_reader {
             2 => Ok(CurveType::Lfo),
             3 => Ok(CurveType::Graph),
             7 => Ok(CurveType::Map),
-            _ => Err(FnvReadError::InvalidCurveType),
+            n @ _ => Err(FnvReadError {
+                kind: FnvReadErrorKind::InvalidCurveType(*n),
+            }),
         }
     }
 }
